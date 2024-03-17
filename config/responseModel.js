5e87dd7acc9c1cd4d19197595f1eaa5d2084e8bc
firozstack1988 <152399106+firozstack1988@users.noreply.github.com>
2024-03-17 17:17:01 +0600

    Add files via upload

---
+++ config/responseModel.js
@@ -0,0 +1,9 @@
+const responseData={
+    success:"success",
+    passMismatch:"Password Not Matching",
+    content:"",
+    userError:"Invalid User",
+    logout:"Successfully Logout"
+ }
+
+ module.exports= responseData;
