fbacc30a09e409da084cca9a00aa9a5175f0ebe3
firozstack1988 <152399106+firozstack1988@users.noreply.github.com>
2024-03-17 17:20:53 +0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 const express= require("express");
 const app=express();
 const cors= require("cors");
+const cookieParser = require('cookie-parser');
 var corsOption={
-    origin :"http://localhost:3000"
+    origin :"http://localhost:3000",
+    method :["POST","GET","DELETE","PUT"],
+    credentials:true
 }
  app.use(cors(corsOption));
  app.use(express.json());
  app.use(express.urlencoded({extended:true}));
 
  require("dotenv").config();
-const port=process.env.port;
+ const port=process.env.port;
+ app.use(cookieParser());
   
  app.use("/api/users",require("./routes/userRoute"));
  app.use("/api/holiday",require("./routes/holidayRoute"));
+ app.use("/api/branchConfig",require("./routes/branchConfigRoute"));
 
  
 app.listen(port,()=>{ 
