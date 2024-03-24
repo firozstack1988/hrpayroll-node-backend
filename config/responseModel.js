6099a483f2e262b6613637641d08e15ad9511362
firozstack1988 <152399106+firozstack1988@users.noreply.github.com>
2024-03-24 14:43:58 +0600

    Add files via upload

--- config/responseModel.js
+++ config/responseModel.js
@@ -6,7 +6,13 @@ const responseData={
     logout:"Successfully Logout",
     PASSWORD_SUCCESS_MSG:"Password changed successfully",
     PASSWORD_FAILURE_MSG:"Password changed failure",
-    NEWPASSWORD_CONFIRMPASSWORD:"New password and confirm password must be same"
+    NEWPASSWORD_CONFIRMPASSWORD:"New password and confirm password must be same",
+    INSERT_SUCCESS:"Successfully inserted",
+    INSERT_FAILURE:"Record not inserted",
+    NOT_FOUND:"No data found",
+    DATA_FOUND:"Data found",
+    UPDATE_SUCCESS:"Data successfully updated",
+    UPDATE_FAILURE:"Data update failure"
  }
 
  module.exports= responseData;
