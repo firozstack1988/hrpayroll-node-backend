982f41228c652ea690ac7c608f1780892cddbbef
firozstack1988 <152399106+firozstack1988@users.noreply.github.com>
2024-03-29 20:39:43 +0600

    Add files via upload

--- routes/employeeRoute.js
+++ routes/employeeRoute.js
@@ -4,11 +4,13 @@ const router = express.Router();
 const {getEmployee,
       createEmployee,
       getEmployeeById,
-      updateEmployee}= require("../controller/employeeController");
+      updateEmployee,
+      getEmployeeByloginId}= require("../controller/employeeController");
 
 router.route("/").get(getEmployee);
 router.route("/").post(createEmployee);
 router.route("/:id").get(getEmployeeById);
+router.route("/empDetail/:id").get(getEmployeeByloginId);
 router.route("/modifyEmp").post(updateEmployee);
 
 module.exports=router;
