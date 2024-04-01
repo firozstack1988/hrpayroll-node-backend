966c42483a65cc0f7d368e2638a12ea86d1cd3fb
firozstack1988 <152399106+firozstack1988@users.noreply.github.com>
2024-04-01 15:22:08 +0600

    Add files via upload

--- routes/leaveRoute.js
+++ routes/leaveRoute.js
@@ -4,12 +4,16 @@ const router = express.Router();
 const {getLeave,
       createLeave,
       getLeaveById,
-      getLeaveStatusByEmpId}= require("../controller/leaveController");
+      getLeaveStatusByEmpId,
+      getLeaveAprovedByAdmin,
+      createStatusUpdate}= require("../controller/leaveController");
 
 router.route("/").get(getLeave);
 router.route("/").post(createLeave);
 router.route("/:id").get(getLeaveById);
 router.route("/leaveStatus/:id").get(getLeaveStatusByEmpId);
+router.route("/leaveAproved/:id").get(getLeaveAprovedByAdmin);
+router.route("/leaveAprovedData").post(createStatusUpdate);
  
 
 module.exports=router;
